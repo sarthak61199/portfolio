51a6f56b391ce0439cd9daae67ff1fe1abd94b65
Sarthak Agarwal <sarthakroxxx99@gmail.com>
2024-06-07 13:52:42 +0530

    added favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import Nav from "@/components/nav";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sarthak Agarwal",
   description:
-    "Sarthak Agarwal's personal website. I'm a software engineer based in India. I love building things for the web. Proficient in React, TypeScript, Node.js and SQL. Currently working at Incedo as a Senior Software Engineer working on both Frontend and Backend technologies. I have around 3 years of experience. in the software industry. Taking complete ownership from Design to Deployment is what I strive for.",
+    "Sarthak Agarwal's personal website. I'm a software engineer based in India. I love building things for the web. Proficient in React.js, Next.js, TypeScript, Node.js and SQL. Currently working at Incedo as a Senior Software Engineer working on both Frontend and Backend technologies. I have around 3 years of experience. in the software industry. Taking complete ownership from Design to Deployment is what I strive for.",
 };
 
 export default function RootLayout({
@@ -19,6 +19,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/icons/favicon-16x16.png"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          rel="icon"
+          href="/icons/favicon-32x32.png"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/icons/apple-touch-icon.png"
+          type="image/png"
+          sizes="180x180"
+        />
+        <link
+          rel="icon"
+          href="/icons/android-chrome-192x192.png"
+          type="image/png"
+          sizes="192x192"
+        />
+        <link
+          rel="icon"
+          href="/icons/android-chrome-512x512.png"
+          type="image/png"
+          sizes="512x512"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={cn(montserrat.className, "min-h-[100svh] flex flex-col")}
       >
